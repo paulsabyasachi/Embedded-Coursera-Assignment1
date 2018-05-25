705d9f4259be65adc71cd4afcdf1170041fa2809
Sabyasachi Paul <sabyasachi.paul@tum.de>
2018-05-25 05:37:43 +0200

    Adding code outline

--- stats.h
+++ stats.h
@@ -14,8 +14,8 @@
  *
  * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Sabyasachi Paul
+ * @date 25.05.2018
  *
  */
 #ifndef __STATS_H__
@@ -35,6 +35,14 @@
  *
  * @return <Add Return Informaiton here>
  */
+
+void print_statistics(float mean, float median, unsigned char min, unsigned char max);
+void print_array(unsigned char *ptr, unsigned int size);
+float find_median(unsigned char *ptr, unsigned int size);
+float find_mean(unsigned char *ptr, unsigned int size);
+unsigned char find_minimum(unsigned char *ptr, unsigned int size);
+unsigned char find_maximum(unsigned char *ptr, unsigned int size);
+void sort_array(unsigned char *ptr, unsigned int size);
 
 
 #endif /* __STATS_H__ */
